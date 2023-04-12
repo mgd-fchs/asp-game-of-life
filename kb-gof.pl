3bc17ab1b3f00a250c90d636d27028df5b9c8083
Magdalena Fuchs <m.hinterkoerner@student.tugraz.at>
2023-04-12 16:46:52 +0200

    Correct initialization

--- kb-gof.pl
+++ kb-gof.pl
@@ -1,5 +1,9 @@
+% Timestep is incremented between 0 and 5
 timestep(0..5).
+timestep(0).
+timestep(T+1) :- timestep(T), T < 5.
 
+% Define valid cells and coordinates on the grid
 value(0).
 value(V+1) :- value(V), size(N), V <= N.
 step(-1).
@@ -9,7 +13,7 @@ diff(X,0) :- step(X).
 diff(0,Y) :- step(Y).
 diff(X,Y) :- step(X), step(Y).
 
-% neighboring cells differ in x or y coordinates by a maximum of abs(1)
+% Neighboring cells differ in x or y coordinates by a maximum of abs(1)
 cell(X,Y) :- value(X), value(Y), size(N), 0 < X, 0 < Y, X <= N, Y <= N.
 near(X,Y,X+DX,Y+DY) :- value(X), value(Y), diff(DX,DY), cell(X+DX,Y+DY).
 
@@ -20,7 +24,7 @@ border_cell(X, Y) :- cell(X, Y), Y == size(N).
 
 { lives(X,Y,T) } :- cell(X,Y), timestep(T).
 lives(X,Y,T) | notlives(X,Y,T) :- cell(X,Y), timestep(T).
-lives(X,Y,T) | -lives(X,Y,T) :- cell(X,Y), timestep(T).
+lives(X,Y,T) | not lives(X,Y,T) :- cell(X,Y), timestep(T).
 
 active(X,Y,X,Y,T) :- lives(X,Y,T), timestep(T).
 active(X,Y,XX,YY,T) :- near(X,Y,XX,YY), lives(XX,YY,T), timestep(T).
@@ -31,13 +35,13 @@ loneliness(X,Y,T) :- cell(X,Y), timestep(T), 2 > #count{ XX,YY : active(X,Y,XX,Y
 preservation(X,Y,T) :- cell(X, Y), timestep(T), 2 == #count{ XX,YY : active(X,Y,XX,YY,T), timestep(T) }.
 preservation(X,Y,T) :- cell(X, Y), timestep(T), 3 == #count{ XX,YY : active(X,Y,XX,YY,T), timestep(T) }.
 
--lives(X,Y,T) :- cell(X,Y), lives(X,Y,T), overpopulation(X,Y,T).
--lives(X,Y,T) :- border_cell(X,Y), timestep(T).
--lives(X,Y,T) :- cell(X,Y), lives(X,Y,T), loneliness(X,Y,T).
-lives(X,Y,T) :- cell(X,Y), lives(X,Y,T), preservation(X,Y,T).
-lives(X,Y,T) :- cell(X,Y), not lives(X,Y,T), birth(X,Y,T).
+not lives(X,Y,T+1) :- cell(X,Y), lives(X,Y,T), overpopulation(X,Y,T).
+not lives(X,Y,T+1) :- border_cell(X,Y), timestep(T+1).
+not lives(X,Y,T) :- border_cell(X,Y), timestep(T).
+not lives(X,Y,T+1) :- cell(X,Y), lives(X,Y,T), loneliness(X,Y,T).
+lives(X,Y,T+1) :- cell(X,Y), lives(X,Y,T), preservation(X,Y,T).
+lives(X,Y,T+1) :- cell(X,Y), not lives(X,Y,T), birth(X,Y,T).
 
-neighbors(X,Y,XX,YY) :- near(X,Y,XX,YY), lives(X,Y,T), timestep(T).
-neighbors(X,Y,XX,YY) :- near(X,Y,XX,YY), cell(X,Y), not lives(XX,YY,T), timestep(T).
 
-%TODO: Introduce a timestep (e.g. add third argument to lives() which is an integer for the timestep)
+%neighbors(X,Y,XX,YY) :- near(X,Y,XX,YY), cell(X,Y), lives(X,Y,T), timestep(T).
+%neighbors(X,Y,XX,YY) :- near(X,Y,XX,YY), cell(X,Y), -lives(XX,YY,T), timestep(T).
--- question1-gof.pl
+++ question1-gof.pl
@@ -4,19 +4,40 @@
 % All other cells are dead
 
 % Define the grid size
+
 size(5).
 
 % Define the alive cells
 % Center cell and cells to its right and left are alive
-lives(3,2).
-lives(3,3).
-lives(3,4).
+lives(3,2,0).
+lives(3,3,0).
+lives(3,4,0).
 
-%#show lives/2.
-%#show initial/2.
-%#show.
+not lives(1,1,0).
+not lives(1,2,0).
+not lives(1,3,0).
+not lives(1,4,0).
+not lives(1,5,0).
 
-#show lives/2.
-%#show border_cell/2.
-%#show neighbors/4.
-%#show active/4.
+not lives(2,1,0).
+not lives(2,2,0).
+not lives(2,3,0).
+not lives(2,4,0).
+not lives(2,5,0).
+
+not lives(4,1,0).
+not lives(4,2,0).
+not lives(4,3,0).
+not lives(4,4,0).
+not lives(4,5,0).
+
+not lives(5,1,0).
+not lives(5,2,0).
+not lives(5,3,0).
+not lives(5,4,0).
+not lives(5,5,0).
+
+not lives(3,1,0).
+not lives(3,5,0).
+
+#show lives/3.
