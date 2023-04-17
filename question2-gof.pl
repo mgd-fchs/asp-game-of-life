8682dd9cd3fb9505103b97f6cdc3a6672480e55b
Magdalena Fuchs <m.hinterkoerner@student.tugraz.at>
2023-04-17 21:05:35 +0200

    Fix Q2

--- question2-gof.pl
+++ question2-gof.pl
@@ -2,15 +2,14 @@
 
 % Define the grid size and timestep
 #const n = 5.
-#const t = 14.
+#const t = 5.
 
 % Axioms to find all configurations which are constantly the same throughout several timesteps:
-alive(X,Y) :- cell(X,Y), lives(X,Y,T), lives(X,Y,T+1), timestep(T).
-dead(X,Y) :- cell(X,Y), not lives(X,Y,T), not lives(X,Y,T+1), timestep(T).
-not alive(X,Y) :- cell(X,Y), lives(X,Y,T), not lives(X,Y,T+1), timestep(T).
-not dead(X,Y) :- cell(X,Y), not lives(X,Y,T), lives(X,Y,T+1), timestep(T).
+alive(X,Y) :- cell(X,Y), lives(X,Y,T), timestep(T).
+dead(X,Y) :- cell(X,Y), not lives(X,Y,T), timestep(T).
 
-% TODO: Encode in configuration (one board for all cells per timestep) and compare those
+:- cell(X,Y), lives(X,Y,T), not lives(X,Y,T+1), timestep(T).
+:- cell(X,Y), not lives(X,Y,T), lives(X,Y,T+1), timestep(T).
 
 #show alive/2.
 #show dead/2.
